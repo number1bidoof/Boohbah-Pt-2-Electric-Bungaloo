41d01cf6a4ac6d63ba756c465410a7c1679c3b04
anabdalla1 <anabdalla1@my.waketech.edu>
2025-11-06 09:22:31 -0500

    No lie gang boohbah might be ruined

---
+++ Humbah.java
@@ -0,0 +1,8 @@
+public class Humbah extends  SupremeBoohbah{
+
+    @Override
+    public void command(){
+        super.command();
+        System.out.println("Humbah leads a warm up to honor ZingZingZingBah");
+    }
+}
---
+++ SupremeBoohbah.java
@@ -0,0 +1,20 @@
+public class SupremeBoohbah {
+    protected String name;
+
+    SupremeBoohbah(){
+        name = "Zing Zing Zingbah";
+        System.out.println("ZingZingZingbah, the supreme Boohbah leader has awaken!!! ");
+
+    }
+    public void command(){
+        System.out.println("All hail the new leader, ZINGZINGZINGBAH");
+    }
+
+    public void command(String text){
+        System.out.println(text);
+    }
+/*    @Override
+    public String toString(String text){
+       return text;
+    }*/
+}
